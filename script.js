277c59126a36503abcbf33b0f46852d02ef71ae1
nimrashahid20 <nimrashahid02007@gmail.com>
2024-07-10 15:47:06 +0500

    while loop
    make functions using while loops in different  ways.

---
+++ script.js
@@ -0,0 +1,429 @@
+
+
+
+
+// let username = prompt("your name is?");
+// console.log(username);
+// let a =33;
+// console.log(a++);
+// console.log(a++);
+// console.log(a++);
+// let email = prompt("enter email");
+// console.log(email);
+// let number1 = +prompt("enter your age");
+// console.log(number1);
+// let y = 12;
+// let z = 18;
+// console.log(z!==y);
+// let mode ="dark";
+// let color ="black";
+// if(mode=="dark"){
+//     console.log(color)
+// }
+// else{
+//     console.log("white")
+// }
+// let age = +prompt("enter age");
+// if(age>18){
+//     console.log("you are not eligible for voting")
+// }
+// else{
+//     console.log ("you are not eligibile for voting")
+// }
+// let number = +prompt("enter your name");
+// if(number % 2 == 0){
+//     console.log(number,"is a even number");
+// }
+// else{
+//     console.log(number,"is a odd number");
+// }
+
+// let num = +prompt("enter num")
+// if(num % 5 == 0){
+// console.log(num,"is a multiple of 5")
+// }
+// else{
+//     console.log(num,"is not a multiple of 5")
+// }
+
+     
+
+
+//question:01//
+// let age = +prompt("enter age");
+// if(age>18){
+//     console.log("you are eligible for voting");
+// }
+// else{
+//     console.log("you are not eligible for voting");
+// }
+//   //question:02//
+// let temperature = +prompt("temperature in celsius is...");
+// if(temperature>25){
+//     console.log("It's a hot day");
+// }
+// else{
+//     console.log("It's a normal day");
+// }
+
+// //question:03//
+// let grade = +prompt("student's student score in a test");
+// if(grade=>60){
+//     console.log("you passed the test");
+// }
+// else{
+//     console.log("you faild the test");
+// }
+
+// //question:04//
+// let raining = true;
+// if(raining == true){
+//     console.log("remember to bring an umbrella");
+// }
+// else{
+//     console.log("enjoy the sunny weather")
+// }
+
+
+// //quetion:05//
+// let birth = +prompt("what is your age");
+// if(birth>18){
+//     console.log("you are an adult");
+// }
+// else{
+//     console.log("you are a minor");
+// }
+
+// //question:06//
+// let number = +prompt("enter number")
+// if(number>0){
+//     console.log("the number is positive")
+// }
+// else if(number<0){
+//     console.log("the number is negative")
+// }
+// else if(number=0){
+//     console.log("the number is zero")
+// }
+
+// let weight = +prompt("enter weight");
+// let age = +prompt("enter age");
+// let time = +prompt("enter time");
+// let gender = prompt("male")
+// if(weight>300 && age>18 && time<7 && gender === "male"){
+//     alert("come to our tryout")
+// }
+// else{
+//     alert("come to our cookout")
+// }
+
+// let x = 10;
+// let y = 5;
+
+// if (x > 5) {
+//     if (y > 2) {
+//         console.log("Both x and y are greater than their respective thresholds.");
+//     } else {
+//         console.log("x is greater than 5, but y is not greater than 2.");
+//     }
+// } else {
+//     console.log("x is not greater than 5, so the inner condition is not checked.");
+// }
+
+
+
+// let temperature = 15;
+// let isSunny = true;
+
+// if(temperature > 25){
+//   console.log("It's hot outside!");
+
+//   if(isSunny){
+//       console.log("Don't forget to wear sunscreen.");
+//   }
+//   else{
+//     console.log("You might still want to wear sunscreen.");
+//   }
+// }
+// else if(temperature >= 15 && temperature <= 25){
+//     console.log("It's a pleasant day.");
+//   if(isSunny){
+//     console.log("Enjoy the sunshine!");
+//   }
+//   else{
+//     console.log("Even if it's cloudy, it's still nice outside.");
+// }
+// }
+// else{
+//     console.log("It's a bit chilly.");
+//     if (isSunny) {
+//         console.log("Don't be fooled by the sun, it's still cool.");
+//     } else {
+//         console.log("It might be cloudy, so bring a jacket just in case.");
+//     }
+// }
+
+
+//      // question :01 //
+// let city = prompt("enter city");
+
+// if(city === "karachi"){
+//     console.log("welcome to city of karachi");
+// }
+// else{
+//     console.log("welcome the city");
+// }
+     
+//       //question :02 //
+    
+// let gender = prompt("enter gender");
+// if(gender=="male"){
+//     console.log("good morning sir");
+// }
+// else if(gender=="female"){
+//     console.log("good morning mam");
+// }
+// else{
+//     console.log("good morning");
+// }
+
+//      //question :03 //
+
+// let color = prompt("enter color");
+// if(color === "red"){
+//     console.log("Must stop")
+// }
+//  else if(color === "yellow"){
+//     console.log("Ready to move");
+//  }
+// else if(color==="green"){
+//     console.log("Move now")
+// }
+//                                                           //arrays start //
+// let name =["nimra" , "kashaf" ,"zarqa" , "maryum", "asra"]
+// console.log(name)
+// console.log(name[3].length)
+// console.log(name.toString())
+
+// let number =[1,2,3,5,7]
+// console.log(number)
+
+// let myGirls = ["laiba","yusra"]
+// let myBoys = ["Rafay","ahmed","mubeen"]
+// let myChildren = myBoys.concat(myGirls)
+// console.log(myBoys)
+// console.log(myGirls)
+// console.log(myChildren)
+
+
+// let fruitss = ["apple","mango","banana","orange"]
+// fruitss.unshift("lemon","juice",45)
+// console.log(fruitss)
+
+
+//  let fruits = ["apple","mango","banana","orange"]
+// console.log(fruits)
+// console.log(fruits.shift())
+// fruits.shift()
+// console.log(fruits)
+// console.log(fruits.toString())
+
+// let fruits = ["apple","mango","banana","orange"]
+// console.log(fruits)
+// let piece_0f_array = fruits.slice(2,4)
+// console.log(piece_0f_array)
+
+// let months =  ["january","febuary","monday","saturday","friday","thursday"]
+// let days = months.splice(3,4, "december")
+// console.log(days)
+// console.log(months)
+
+//question :01
+
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"]
+// console.log(companies)
+// companies.shift("blooberg")
+// console.log(companies)
+// let olo = companies.splice(1,1,"Ola")
+// console.log(olo)
+// console.log(companies)
+// companies.push("Amazon")
+// console.log(companies)
+
+//question :01//
+// let studentName = [];
+// console.log(studentName)
+
+
+// //question :02
+
+// let string = ["nimra","kashaf","zarqa","fatima"] Upgrade Feedback Login / Register
+// Text
+// console.log(string)
+
+
+// //question:03
+// let num = [ 2,3,34,5,6,]
+// console.log(num)
+
+// //question:04
+
+// let blue = [true,false]
+// console.log(blue)
+
+// //question:05
+
+// let mixed = ["netflix",true,22]
+// console.log(mixed)
+
+// //question :06
+
+// let edu = ["SSC","HSC","BSC","BS","B.COM","MS","M.PHIL","PhD"]
+// document.write(" qualification")
+// document.write(edu)
+
+//question :07
+
+// let studentName = ["Michael","John","Tony"]
+// let studentScore = [435,345,234]
+// let total = 500
+// let percentage1 = [435/500*100]
+// let percentage2 = [345/500*100]
+// let percentage3 = [234/500*100]
+
+// document.write("<br>"+studentName[0] +"  "+"has sucessfully acheived the percentage"+"   "+percentage1 +"%")
+// document.write("<br>"+studentName[1] +"  "+"has sucessfully acheived the percentage"+"   "+percentage2+"%")
+// document.write("<br>"+studentName[2] +"  "+"has acheived the percentage"+"   "+percentage3+"%")
+
+// for(let i = 0;i<10;i++){
+//     console.log("hello world")
+// }
+// for(let i = 14;i<140;i +=14){
+//     console.log("thetable of 14"+i)
+// }
+
+
+                                        //function//
+// let firstName = prompt("enter firstname");
+// let lastName = prompt("enter last name");
+// function greats(firstName,lastName){
+//     document.write(firstName,lastName)
+// }
+// greats(firstName,lastName)
+
+// let num1 = +prompt("enter a number");
+// let num2 = +prompt("enter  a number")
+// function num(num1,num2){
+//     return (num1+num2)
+// }
+// console.log(num(num1,num2))
+
+
+// let num1 = +prompt("enter a number")
+// let num2 = +prompt("enter a number")
+// let operator = prompt("enter a operator")
+// function calculator(num1,num2,operator){
+//     if(operator === "+"){
+//         document.write(num1 +"  "+operator+"  "+num2+"  "+"  "+"="+(num1+num2))
+//     }
+//     else if(operator === "-"){
+//         document.write(num1 +"  "+operator+"  "+num2+"  "+"  "+"="+(num1-num2))
+//     }
+//     else if(operator === "*"){
+//         document.write(num1 +"  "+operator+"  "+num2+"  "+"   "+"="+(num1*num2))
+//     }
+//     else if(operator === "/"){
+//         document.write(num1 +"  "+operator+"  "+num2+"  "+"    "+"="+(num1/num2))
+//     }
+
+
+
+// }
+// calculator(num1,num2, operator)
+
+
+// function square(num){
+// return(num*num)
+// }
+// console.log(square(4))
+
+//  
+
+
+// let start_number = +prompt("enter a number")
+// let c= +prompt("enter a number")
+// function count(start_number ,end_number ){
+//     if(start_number>end_number){
+//         for(let i =start_number ; i> end_number ;i--)
+// document.write(i+"<br>") }
+// else if(start_number<=end_number){
+//     for(let i =start_number ; i<=end_number ;i++)
+// document.write(i+"<br>") } 
+
+// }
+
+// count(start_number,end_number)
+
+// function calculateDiscount(producttype,price{
+    
+// })
+
+
+                                     //while loop 
+
+// let n =+prompt("enter a number") 
+// let i = 0 
+// while(i<=n){
+//     document.write("<br>"+i);
+//     i++
+// }
+
+
+// let sum = 0;
+// let num = 1;
+// while(num+=100){
+//     sum+=num;
+//     i++
+// }
+// document.write("sum of 1 to 100:" + sum)
+
+// let number = 4;
+// let factorial = 1;
+// while(number>0){
+//     factorial = factorial*number
+//     i--
+// }
+// document.write("factorial of a number"+factorial)
+
+
+let evennumber = [];
+let i = 0;
+while(evennumber.length<10){
+    if(i%2===0){
+        evennumber.push(i)
+    }
+    i++
+}
+document.write(evennumber)
+
+let userinput;
+let password ="javascrpit123"
+while(userinput!==password){
+    userinput
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
